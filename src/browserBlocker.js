72a9635dd77f20a2435fd7c7db2ae4fed8e673cc
JaydenDev <willsjayden2@gmail.com>
2022-04-21 21:20:55 -0500

    Apple watch detection/ Bug Fixes

--- src/browserBlocker.js
+++ src/browserBlocker.js
@@ -1,27 +1,29 @@
 // the config
 browserBlockedPage = "html/blocked.html";
 unsupportedBrowsers = ["IE", "edge"];
-unsupportedBrowserAction = block; // options: block or warn
+unsupportedBrowserAction = "block"; // options: block or warn
 
 function detectBrowser() {
     if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
         browser = "opera";
-    }
-    else if (navigator.userAgent.indexOf("Edg") != -1) {
+    } else if (navigator.userAgent.indexOf("Edg") != -1) {
         browser = "edge;"
-    }
-    else if (navigator.userAgent.indexOf("Chrome") != -1) {
+    } else if (navigator.userAgent.indexOf("Chrome") != -1) {
         browser = "chrome";
-    }
-    else if (navigator.userAgent.indexOf("Safari") != -1) {
+    } else if (navigator.userAgent.indexOf("Safari") != -1) {
         browser = "safari";
-    }
-    else if (navigator.userAgent.indexOf("Firefox") != -1) {
+    } else if (navigator.userAgent.indexOf("Firefox") != -1) {
         browser = "firefox";
-    }
-    else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) //IF IE > 10
+    } else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) //IF IE > 10
     {
-       browser = "IE";
+        browser = "IE";
+    }
+    let a = document.createElement('audio');
+    let supportsAAC = !!(a.canPlayType && a.canPlayType('audio/mp4; codecs="mp4a.40.2"').replace(/no/, ''));
+    let is_iphone = ['iPhone Simulator', 'iPhone'].includes(navigator.platform)
+    return is_iphone && !supportsAAC
+    if (is_iphone && !supportsAAC) {
+        browser = "appleWatch";
     }
 }
 
